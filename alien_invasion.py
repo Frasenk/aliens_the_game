a740f9296905b24f096aa124bce4f2bfc0207743
Frasenk <kubos7@gmail.com>
2020-04-25 18:38:43 +0200

    add ship class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
 
@@ -12,6 +13,7 @@ def run_game():
     )
 
     pygame.display.set_caption("Gierka xD")
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
@@ -19,6 +21,8 @@ def run_game():
                 sys.exit()
 
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         pygame.display.flip()
 
 run_game()
