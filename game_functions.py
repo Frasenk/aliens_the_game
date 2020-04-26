1dcf5779ecdb3720261f490f1ce43d0edee66465
Frasenk <kubos7@gmail.com>
2020-04-26 14:11:33 +0200

    added moving ship x1.5 speed in frames, refactor game_functions

--- game_functions.py
+++ game_functions.py
@@ -3,23 +3,30 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-        elif event.type == pygame.KEYDOWN:      # check if key is pressed
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+        elif event.type == pygame.KEYDOWN:  # check if key is pressed
+            check_keydown_events(event, ship)
 
-        elif event.type == pygame.KEYUP:        # check if key is up
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-
+        elif event.type == pygame.KEYUP:  # check if key is up
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
