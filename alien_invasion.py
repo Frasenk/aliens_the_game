df324cc4dbf48bcda9434672b5de1c28b80e5718
Frasenk <kubos7@gmail.com>
2020-04-27 20:07:10 +0200

    created alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 
 import game_functions as gf
 
@@ -19,13 +18,14 @@ def run_game():
     pygame.display.set_caption("Gierka xD")
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    alien = Alien(ai_settings, screen)
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
